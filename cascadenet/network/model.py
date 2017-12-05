32c5fb1d40fdffa8c51687fb476038d26c82084e
Ray <tinjuiho@umich.edu>
2017-12-05 15:27:06 -0500

    try pretrained2

--- cascadenet/network/model.py
+++ cascadenet/network/model.py
@@ -9,7 +9,7 @@ from collections import OrderedDict
 def cascade_resnet(pr, net, input_layer, n=5, nf=64, b=lasagne.init.Constant, **kwargs):
     shape = lasagne.layers.get_output_shape(input_layer)
     n_channel = shape[1]
-    net[pr+'conv1'] = batch_norm(l.Conv(input_layer, nf, 3, b=b(), name=pr+'conv1'))
+    net[pr+'conv1'] = l.Conv(input_layer, nf, 3, b=b(), name=pr+'conv1')
 
     for i in xrange(2, n):
         net[pr+'conv%d'%i] = batch_norm(l.Conv(net[pr+'conv%d'%(i-1)], nf, 3, b=b(),
