d96fd72674e50d6f228c457d5f89ca3775569647
Ray <tinjuiho@umich.edu>
2017-12-14 22:00:23 -0500

    try trans conv

--- cascadenet/network/model.py
+++ cascadenet/network/model.py
@@ -2,6 +2,7 @@ import lasagne
 import cascadenet.network.layers as l
 
 from lasagne.layers import batch_norm 
+from lasagne.layers import TransposedConv2DLayer as transConv
 
 from collections import OrderedDict
 
@@ -13,6 +14,8 @@ def cascade_resnet_modified(pr, net, input_layer, n=5, nf=64, b=lasagne.init.Con
     net[pr+'conv2'] = l.Conv(net[pr+'conv1'], nf, 3, b=b(), name=pr+'conv2')
     net[pr+'conv3'] = l.Conv(net[pr+'conv2'], nf, 3, b=b(), name=pr+'conv3')
     net[pr+'conv4'] = l.Conv(net[pr+'conv3'], nf, 3, b=b(), name=pr+'conv4')
+    net[pr+'transConv1'] = transConv(net[pr+'conv4'], net[pr+'conv4'].input_shape[1], net[pr+'conv4'].filter_size, stride=net[pr+'conv4'].stride, crop=net[pr+'conv4'].pad, W=net[pr+'conv4'].W, flip_filters=not net[pr+'conv4'].flip_filters)
+
     net[pr+'conv_aggr'] = l.ConvAggr(net[pr+'conv4'], n_channel, 3, b=b(), name=pr+'conv_aggr')
     net[pr+'res'] = l.ResidualLayer([net[pr+'conv_aggr'], input_layer], name=pr+'res')
     output_layer = net[pr+'res']
@@ -109,8 +112,8 @@ def build_d2_c2(shape):
 
 
 def build_d5_c5(shape):
+    # return build_cascade_cnn_from_list(shape, [(cascade_resnet, 5)])
     return build_cascade_cnn_from_list(shape, [(cascade_resnet_modified, 5)])
-
 
 def build_d2_c2_s(shape):
     def cascade_d2(pr, net, input_layer, **kwargs):
