98728444d9f459a73bf22ae2a2bc8f1a2c161149
Ray <tinjuiho@umich.edu>
2017-12-16 15:52:13 -0500

    kernelsize 11 acc 8 ready to test

--- cascadenet/network/model.py
+++ cascadenet/network/model.py
@@ -36,16 +36,16 @@ from collections import OrderedDict
 #     return net, output_layer
 
 
-def cascade_resnet(pr, net, input_layer, n=20, nf=64, b=lasagne.init.Constant, **kwargs):
+def cascade_resnet(pr, net, input_layer, n=5, nf=64, b=lasagne.init.Constant, **kwargs):
     shape = lasagne.layers.get_output_shape(input_layer)
     n_channel = shape[1]
-    net[pr+'conv1'] = l.Conv(input_layer, nf, 3, b=b(), name=pr+'conv1')
+    net[pr+'conv1'] = l.Conv(input_layer, nf, 11, b=b(), name=pr+'conv1')
 
     for i in xrange(2, n):
-        net[pr+'conv%d'%i] = l.Conv(net[pr+'conv%d'%(i-1)], nf, 3, b=b(),
+        net[pr+'conv%d'%i] = l.Conv(net[pr+'conv%d'%(i-1)], nf, 11, b=b(),
                                     name=pr+'conv%d'%i)
 
-    net[pr+'conv_aggr'] = l.ConvAggr(net[pr+'conv%d'%(n-1)], n_channel, 3,
+    net[pr+'conv_aggr'] = l.ConvAggr(net[pr+'conv%d'%(n-1)], n_channel, 11,
                                      b=b(), name=pr+'conv_aggr')
     net[pr+'res'] = l.ResidualLayer([net[pr+'conv_aggr'], input_layer],
                                     name=pr+'res')
